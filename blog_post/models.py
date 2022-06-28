cb49656ddd90ac26bfb501e87da7efc67152a513
Wallas Curty <65136877+WallasCurty@users.noreply.github.com>
2022-06-28 09:10:19 -0300

    Post
    Blog com o post já ativo

---
+++ blog_post/models.py
@@ -0,0 +1,12 @@
+from turtle import title
+from venv import create 
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=20)
+    resumo = models.CharField(max_length=255)
+    conteudo = models.TextField()
+    autor = models.ForeignKey(User, on_delete=models.PROTECT)
+    data_criacao = models.DateField(auto_now_add=True)
